a4615a6f742410cba9d1d4bc4ef96cfcf25473ba
Areri <you@example.com>
2021-03-17 16:34:10 +0300

    add my project

---
+++ app/Models/supliers_product.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class supliers_product extends Model
+{
+    use HasFactory;
+}
